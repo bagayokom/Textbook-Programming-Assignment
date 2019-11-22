3324f86625fb2ff039d9077fd7edca1c5628abee
bagayokom <56162680+bagayokom@users.noreply.github.com>
2019-11-22 02:02:12 -0500

    Create textbooks.py

---
+++ textbooks.py
@@ -0,0 +1,16 @@
+from person import Person
+
+class Textbooks():
+    def __init__(self, title, first, last, age, edition, ISBN, publisher, year, inventory, price):
+        self.title = title
+        self.author = Person(first, last, age)
+        self.edition = edition
+        self.ISBN = ISBN
+        self.publisher = publisher
+        self.year = year
+        self.inventory = inventory
+        self.price = price
+
+    def add_to_inventory(self, qty):
+        self.inventory = self.inventory + qty
+
